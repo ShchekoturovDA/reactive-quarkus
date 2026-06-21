513565691499b5cf2bf5c37afae51a539f50d0ed
ShchekoturovDA <shchekoturovdmitr@gmail.com>
2026-06-22 02:24:48 +0300

    поправил фильтрацию новостей по именам авторов

--- src/main/java/com/shchek/pets/service/HackerNewsService.java
+++ src/main/java/com/shchek/pets/service/HackerNewsService.java
@@ -2,7 +2,6 @@ package com.shchek.pets.service;
 
 import com.shchek.pets.client.HackerNewsClient;
 import com.shchek.pets.dto.response.NewsResponseDTO;
-import com.shchek.pets.entity.Filter;
 import com.shchek.pets.mappers.dto.NewsItemDTOToTopTenResponseMapper;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
@@ -10,7 +9,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-import java.time.Duration;
 import java.util.List;
 
 import static com.shchek.pets.entity.FilterType.AUTHOR;
@@ -46,26 +44,38 @@ public class HackerNewsService {
     }
 
     public Multi<NewsResponseDTO> getTopTitlesByDashboards(List<String> dashboardsNames, Long depth) {
-        Multi<Filter> filterMulti =
-                dashBoardService.getDashBoardsByName(
-                                Uni.createFrom()
-                                        .item(dashboardsNames).onItem()
-                                        .transformToMulti(
-                                                names ->
-                                                        Multi.createFrom().iterable(names))
-                        ).onItem()
-                        .transformToMultiAndMerge(
-                                dashboard ->
-                                        Multi.createFrom().iterable(dashboard.filters));
+        Multi<String> filterNames = dashBoardService
+                .getDashBoardsByName(
+                        Multi.createFrom().iterable(dashboardsNames))
+                .onItem()
+                .transformToMultiAndMerge(
+                        dashboard ->
+                                Multi.createFrom().iterable(dashboard.filters))
+                .filter(
+                        filter ->
+                                !filter.isReverse && filter.filterType.equals(AUTHOR.name()))
+                .onItem()
+                .transform(filter -> filter.filterName)
+                .select()
+                .distinct();
 
-        Multi<NewsResponseDTO> newsResponseDTOMulti = getTopTitles(depth);
-        newsResponseDTOMulti.subscribe().with(
-                news ->
-                        filterMulti.filter(
-                                        filter ->
-                                                filter.filterType.equals(AUTHOR.name())
-                                                        && filter.filterName.equals(news.getAuthor()))
-                                .subscribe().with(result -> news.setShow(true)));
-        return newsResponseDTOMulti.select().where(NewsResponseDTO::isShow);
+
+        Multi<NewsResponseDTO> topNews = getTopTitles(depth);
+
+        return topNews.onItem().transformToUniAndMerge(
+                        news ->
+                                filterNames.select().where(
+                                                fName ->
+                                                        fName.equals(news.getAuthor()))
+                                        .toUni()
+                                        .onItem()
+                                        .transform(
+                                                first -> {
+                                                    news.setShow(first != null);
+                                                    return news;
+                                                }))
+                .select()
+                .where(NewsResponseDTO::isShow);
     }
 }
+
